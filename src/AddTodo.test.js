da0a3d9dc1c4c5110c3b0963a526a8a905385b1e
kunsang tsering <kunsangtsering@Dheerajs-iMac.local>
2024-06-21 21:30:45 -0400

    Final Commit With All Changes

--- src/AddTodo.test.js
+++ src/AddTodo.test.js
@@ -1,43 +1,72 @@
-import { render, screen, fireEvent} from '@testing-library/react';
-import { unmountComponentAtNode } from 'react-dom';
-import App from './App';
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import AddTodo from "../component/AddTodo"; 
+import '@testing-library/jest-dom/extend-expect'; 
 
-let container = null;
-beforeEach(() => {
-  // setup a DOM element as a render target
-  container = document.createElement("div");
-  document.body.appendChild(container);
+describe('AddTodo component', () => {
+  test('adds a task when the add button is clicked', () => {
+    const addTodo = jest.fn();
+    render(<AddTodo addTodo={addTodo} />);
+    
+    const inputTask = screen.getByLabelText(/Add New Item/i);
+    const inputDate = screen.getByLabelText(/Due Date/i);
+    const addButton = screen.getByTestId('new-item-button');
+
+    fireEvent.change(inputTask, { target: { value: 'New Task' } });
+    fireEvent.change(inputDate, { target: { value: '05/30/2023' } });
+    fireEvent.click(addButton);
+
+    expect(addTodo).toHaveBeenCalledWith({
+      content: 'New Task',
+      date: expect.any(String),
+      due: '5/30/2023'
+    });
+  });
+
+  test('does not add duplicate tasks', () => {
+    const addTodo = jest.fn();
+    render(<AddTodo addTodo={addTodo} />);
+    
+    const inputTask = screen.getByLabelText(/Add New Item/i);
+    const inputDate = screen.getByLabelText(/Due Date/i);
+    const addButton = screen.getByTestId('new-item-button');
+
+    fireEvent.change(inputTask, { target: { value: 'New Task' } });
+    fireEvent.change(inputDate, { target: { value: '05/30/2023' } });
+    fireEvent.click(addButton);
+    fireEvent.change(inputTask, { target: { value: 'New Task' } });
+    fireEvent.change(inputDate, { target: { value: '05/30/2023' } });
+    fireEvent.click(addButton);
+
+    expect(addTodo).toHaveBeenCalledTimes(1);
+  });
+
+  test('does not add a task without a due date', () => {
+    const addTodo = jest.fn();
+    render(<AddTodo addTodo={addTodo} />);
+    
+    const inputTask = screen.getByLabelText(/Add New Item/i);
+    const addButton = screen.getByTestId('new-item-button');
+
+    fireEvent.change(inputTask, { target: { value: 'New Task' } });
+    fireEvent.click(addButton);
+
+    expect(addTodo).not.toHaveBeenCalled();
+  });
+
+  test('overdue tasks have a different color', () => {
+    const addTodo = jest.fn();
+    render(<AddTodo addTodo={addTodo} />);
+    
+    const inputTask = screen.getByLabelText(/Add New Item/i);
+    const inputDate = screen.getByLabelText(/Due Date/i);
+    const addButton = screen.getByTestId('new-item-button');
+
+    fireEvent.change(inputTask, { target: { value: 'Overdue Task' } });
+    fireEvent.change(inputDate, { target: { value: '05/30/2022' } });
+    fireEvent.click(addButton);
+
+    const overdueTask = screen.getByText(/Overdue Task/i);
+    expect(overdueTask.closest('div')).toHaveStyle('background-color: #ffcccc');
+  });
 });
-
-afterEach(() => {
-  // cleanup on exiting
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
-
-
-
-
- test('test that App component doesn\'t render dupicate Task', () => {
-  render(<App />);
- });
-
- test('test that App component doesn\'t add a task without task name', () => {
-  render(<App />);
- });
-
- test('test that App component doesn\'t add a task without due date', () => {
-  render(<App />);
- });
-
-
-
- test('test that App component can be deleted thru checkbox', () => {
-  render(<App />);
- });
-
-
- test('test that App component renders different colors for past due events', () => {
-  render(<App />);
- });
